08eba8f29c177dfd6c03109c5d2e07e1f5a3d205
Fauzan <fauzanhabib20@gmail.com>
2025-11-13 15:19:29 +0700

    updated error build

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import FormPageContent from "@/components/FormPageContent";
 import type { Metadata } from "next";
 
@@ -7,5 +8,9 @@ export const metadata: Metadata = {
 };
 
 export default function Page() {
-  return <FormPageContent />;
+  return (
+    <Suspense fallback={<div>Loading booking form...</div>}>
+      <FormPageContent />
+    </Suspense>
+  )
 }
