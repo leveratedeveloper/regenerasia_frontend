19704fdcd4fd7572fa59dfdfc1be229d4c1ce528
leveratedeveloper <developer@leverate.co.id>
2025-12-12 15:19:07 +0700

    update version and wording

--- components/FormPageContentEnquires.tsx
+++ components/FormPageContentEnquires.tsx
@@ -75,7 +75,7 @@ export default function PageContent() {
         <main className="p-6 md:p-12 max-w-5xl mx-auto">
           <div className="text-center mb-8">
             <div className="inline-flex items-center space-x-2 p-1 rounded-lg border border-gray-300">
-              <TabButton label="Business Enquires" tabName="booking" />
+              <TabButton label="Business Enquiries" tabName="booking" />
               {/* <TabButton label="Request for Quotation" tabName="rfq" /> */}
             </div>
           </div>
